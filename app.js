4aaed3aede167c1af65320c3fea3d05511400d16
xuanchu jiang <xuanchujiang@gmail.com>
2023-12-04 03:57:46 -0500

    env

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 app.use(
   session(sessionOptions)
 );
